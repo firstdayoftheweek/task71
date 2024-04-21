f46aea34d65763ad1882b881941a653c129cc958
firstdayoftheweek <151506166+firstdayoftheweek@users.noreply.github.com>
2024-04-21 16:23:45 +0500

    Add files via upload

---
+++ src/main/java/com/example/manytoone/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.manytoone.repository;
+
+import com.example.manytoone.model.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository  extends JpaRepository<Courses,Long> {
+}
